2f7c990b0547f346caaa5bbfb80f0d702033af7e
Nuh AKINCI <101666675+nuhakinci@users.noreply.github.com>
2022-03-15 22:09:21 +0300

    Update App.tsx
    Bridge changed solar.network to solar.org (Footer).

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       </div>
       <div className="mt-8 h-12  border-t-2 border-secondary dark:border-dark-secondary text-center text-white">
         <div className="max-w-7xl mx-auto py-6 text-black dark:text-white">
-            2022 <a href="https://solar.network" className="text-greenish">Solar Network</a> | Market Data by <a href="https://coingecko.com/" className="text-greenish">coingecko</a> 
+            2022 <a href="https://solar.org" className="text-greenish">Solar Network</a> | Market Data by <a href="https://coingecko.com/" className="text-greenish">coingecko</a> 
         </div>
       </div>
       </BlockchainContext.Provider>
